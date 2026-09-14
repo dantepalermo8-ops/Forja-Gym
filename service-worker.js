ce510269d92a6fa54830d598367ffd46cf91e607
dantepalermo8-ops <dantepalermo8@gmail.com>
2026-09-14 20:30:34 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME="forja-v1";
+const FILES_TO_CACHE=["./","./index.html","./manifest.json","./icon-180.png","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
